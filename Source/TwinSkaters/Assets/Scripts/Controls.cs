467035e241f3657f6b02a54cd9744b77ba378d04
LOG <m.einakchi@gmail.com>
2015-12-06 19:19:02 -0500

    Added initial structure for assets, BackgroundController for infinite background scroll and controls for controlling the skates

---
+++ Source/TwinSkaters/Assets/Scripts/Controls.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controls : MonoBehaviour {
+	
+	public float speed = 1.0f;
+		
+	private GameObject leftSkate;
+	private GameObject rightSkate;
+	
+	
+	void Start () 
+	{
+		leftSkate = GameObject.FindWithTag ("LeftSkate");
+		rightSkate = GameObject.FindWithTag ("RightSkate");
+	}
+	
+	void FixedUpdate ()
+	{
+		Vector3 leftSkateMove = Vector3.zero;
+		Vector3 rightSkateMove = Vector3.zero;
+		if (Input.GetKey ("right")) {									
+			leftSkateMove = new Vector3(1,0,0) * speed * Time.deltaTime;
+			rightSkateMove = leftSkateMove;			
+		} else if (Input.GetKey ("left")) {
+			leftSkateMove = new Vector3(-1,0,0) * speed * Time.deltaTime;
+			rightSkateMove = leftSkateMove;			
+		} else if (Input.GetKey ("up")) {
+			leftSkateMove = new Vector3(-1,0,0) * speed * Time.deltaTime;
+			rightSkateMove = new Vector3(1,0,0) * speed * Time.deltaTime;			
+		} else if (Input.GetKey ("down")) {
+			leftSkateMove = new Vector3(1,0,0) * speed * Time.deltaTime;
+			rightSkateMove = new Vector3(-1,0,0) * speed * Time.deltaTime;			
+		}
+		
+		if (leftSkateMove != Vector3.zero) {
+			leftSkate.transform.Translate(leftSkateMove);	
+		}
+		if (rightSkateMove != Vector3.zero) {
+			rightSkate.transform.Translate(rightSkateMove);
+		}
+		
+	}
+
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
