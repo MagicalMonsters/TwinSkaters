9448eb9e26a37661a5e40ef12241750793074efe
Behdad <bakhshi.behdad@gmail.com>
2015-12-07 19:43:26 -0700

    reset time for cones removed

--- Source/TwinSkaters/Assets/Scripts/BackgroundController.cs
+++ Source/TwinSkaters/Assets/Scripts/BackgroundController.cs
@@ -6,31 +6,26 @@ public class BackgroundController : MonoBehaviour
     public float scrollSpeed;
     public float tileSizeY;
 	private GameObject[] cones;
-	public float obsResetTime;
-	private float timer;
 
     private Vector3 startPosition;
 
     void Start ()
     {
         startPosition = transform.position;
-		timer = obsResetTime;
 		if(cones == null)
 			cones = GameObject.FindGameObjectsWithTag ("cone");
     }
 
-    void Update ()
-    {
-        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
-        transform.position = startPosition + Vector3.up * newPosition;
-		timer -= Time.deltaTime;
-		if (timer <= 0.0f) {
-			timer = obsResetTime;
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+		if (transform.position.y < (startPosition + Vector3.up * newPosition).y) {
 			foreach(GameObject cone in cones){
 				cone.SetActive(true);
 				float ny = transform.position.y;
 				cone.transform.position = new Vector3( (Random.value-0.5f)*7.0f, ny, 0.0f);
 			}
 		}
-    }
+		transform.position = startPosition + Vector3.up * newPosition;
+	}
 }
