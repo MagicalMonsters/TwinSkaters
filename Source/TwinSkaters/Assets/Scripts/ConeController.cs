ac97fb29e1f5b12a120e5ec69c5ee26231011e9b
LOG <m.einakchi@gmail.com>
2015-12-07 21:31:45 -0500

    Added ConeController to intercept the collision with skates and created a cone prefab

---
+++ Source/TwinSkaters/Assets/Scripts/ConeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConeController : MonoBehaviour {
+
+	private System.Action onCollision;
+	
+		
+	void SetOnCollision (System.Action onCollision) 
+	{
+		this.onCollision = onCollision;
+	}
+	
+	void OnCollisionEnter (Collision collision) 
+	{
+		if (collision.gameObject.tag == "RightSkate" || collision.gameObject.tag == "LeftSkate" ) 
+		{
+			if (onCollision != null)
+			{
+				onCollision();
+				onCollision = null;
+			}		
+		}		
+	}
+}
