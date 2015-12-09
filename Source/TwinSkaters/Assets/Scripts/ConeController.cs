cff96143875446c2190f65ad88f3ea479a2a2501
LOG <m.einakchi@gmail.com>
2015-12-09 01:09:47 -0500

    Cone rotation and Flasher cones
    Added Red cone image to sprites
    Cones are now generated and controlled by GameController (no static
    cones as GameObjects)
    After some seconds cones appear as rotated

--- Source/TwinSkaters/Assets/Scripts/ConeController.cs
+++ Source/TwinSkaters/Assets/Scripts/ConeController.cs
@@ -3,23 +3,52 @@ using System.Collections;
 
 public class ConeController : MonoBehaviour {
 
+	public Sprite orangeCone;
+	public Sprite redCone;
+	
+	public float spriteSwitchDuration = 0.3f;
+
 	private System.Action onCollision;
 	private Vector3 startPosition;
 	private GameController gameController;
+	private SpriteRenderer spriteRenderer;
+	
+	private bool shouldRemove = false;
+	private bool isRotated = false;
+	private bool isUnstable = false;
+	private float spriteSwitchTime = 0f;
+	private bool spriteSwitchState = false;
 
 	void Start ()
 	{
 		startPosition = transform.position;
 		gameController = GameObject.FindWithTag("GameScript").GetComponent<GameController>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
 	void Update ()
-	{
+	{		
+		if (isUnstable) 
+		{
+			SpriteAnimation();	
+		}		
 		float newPosition = Time.deltaTime * gameController.scrollSpeed;
 		if (transform.position.y < -5.0f) {
+			if (shouldRemove) {
+				gameController = null;
+				Destroy(gameObject);
+				return;
+			}
+			if (isRotated) {
+				transform.Rotate(0,0,Random.Range(0,360));
+			} else if (isUnstable) {
+				
+			}
+			
 			transform.position = new Vector3( (Random.value-0.5f) * 11.0f, startPosition.y, transform.position.z);
 		}
 		transform.position -= Vector3.up * newPosition;
+		
 	}
 		
 	public void SetOnCollision (System.Action onCollision) 
@@ -37,5 +66,29 @@ public class ConeController : MonoBehaviour {
 				onCollision = null;
 			}		
 		}		
+	}
+	
+	public void Remove () 
+	{
+			shouldRemove = true;
+	}
+	
+	public void MakeRotated ()
+	{
+		isRotated = true;
+	}
+	
+	public void MakeUnstable ()
+	{
+		isUnstable = true;
+	}
+	
+	private void SpriteAnimation () {
+		spriteSwitchTime += Time.deltaTime;
+		if (spriteSwitchTime > spriteSwitchDuration) {
+			spriteSwitchTime = 0;
+			spriteRenderer.sprite = (spriteSwitchState) ? redCone : orangeCone;
+			spriteSwitchState = !spriteSwitchState;
+		}
 	}
 }
--- Source/TwinSkaters/Assets/Scripts/GameController.cs
+++ Source/TwinSkaters/Assets/Scripts/GameController.cs
@@ -3,16 +3,36 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+
 	public float scrollSpeed = 5.0f;
-	public UnityEngine.UI.Text centerText;
+	// public float scrollSpeedCoefficient = 0.000001f;
 	
+	public float threshold1 = 10f;
+	public float threshold2 = 50f;
+	
+	public int level1NumberOfCones = 5;
+	
+	public UnityEngine.UI.Text centerText;
+	public GameObject conePrefab;
+	
+	// private float initialScrollSpeed;
 	private bool waitForAnyKeyToRestart = false;
+	private GameObject[] cones;
 
 	void Start () {
-		GameObject[] cones = GameObject.FindGameObjectsWithTag ("Cone");        
+		// initialScrollSpeed = scrollSpeed;
+		cones = new GameObject[level1NumberOfCones];
+		
+		for (int i = 0; i < level1NumberOfCones; i++)
+		{
+			float randX = (Random.value-0.5f) * 11.0f;
+			float randY =  Random.value * 5 + 5;
+			cones[i] = (GameObject) Instantiate(conePrefab,new Vector3 (randX, randY, 0), Quaternion.identity);
+		}
+		        
         foreach (GameObject cone in cones)
         {        
-            ConeController c = cone.GetComponent<ConeController> ();         
+            ConeController c = cone.GetComponent<ConeController> ();			         
             c.SetOnCollision (() => {
 				waitForAnyKeyToRestart = true;
             	Time.timeScale = 0;
@@ -22,6 +42,21 @@ public class GameController : MonoBehaviour {
 	}
 		
 	void Update () {
+		
+		// scrollSpeed = initialScrollSpeed + Time.deltaTime * scrollSpeedCoefficient;
+				
+		if (threshold1 != 0 && threshold1 < Time.timeSinceLevelLoad)
+		{
+			threshold1 = 0;
+			cones[0].GetComponent<ConeController>().MakeRotated();
+			cones[1].GetComponent<ConeController>().MakeRotated();
+        } else if (threshold2 != 0 && threshold2 < Time.timeSinceLevelLoad) 
+		{
+			threshold2 = 0;
+			cones[1].GetComponent<ConeController>().MakeUnstable();
+		}	
+		
+		// handle gameover case
 		if (waitForAnyKeyToRestart)
 		{
 			if (Input.anyKey) {
