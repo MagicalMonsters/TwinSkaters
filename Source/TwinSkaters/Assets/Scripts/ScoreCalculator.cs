e192aa41fd1e1f5f788ac23409ccbde0b65961a1
Behdad <bakhshi.behdad@gmail.com>
2015-12-09 01:31:38 -0700

    scoreEdge added

---
+++ Source/TwinSkaters/Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCalculator : MonoBehaviour {
+
+	public UnityEngine.UI.Text ScoreText;
+	private GameObject LeftSkate,RightSkate;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		LeftSkate = GameObject.FindWithTag ("LeftSkate");
+		RightSkate = GameObject.FindWithTag ("RightSkate");
+		score = 0;
+		ScoreText.text = "0";
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		GameObject collider = other.gameObject;
+		if (collider.tag == "Cone" && collider.transform.position.x < RightSkate.transform.position.x &&
+		    collider.transform.position.x > LeftSkate.transform.position.x ) {
+			score++;
+			ScoreText.text = ""+score;
+		}
+	}
+}
